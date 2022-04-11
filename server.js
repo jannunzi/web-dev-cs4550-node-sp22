f819c44ee3a01dd1a95f0702bfc32c004f3d46a6
JoseAnnunziato <jose.annunziato@sonos.com>
2022-04-10 23:42:12 -0400

    db connect

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
-mongoose.connect('mongodb://localhost:27017/webdev');
-
+const DB_USERNAME = process.env.DB_USERNAME
+const DB_PASSWORD = process.env.DB_PASSWORD
+// mongoose.connect(`mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster0.m8jeh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`);
+mongoose.connect('mongodb://localhost:27017/cs4550-sp22');
+//
 // const tuitsSchema = mongoose.Schema({
 //   tuit: String,
 //   likes: {type: Number, defaultValue: 0}
